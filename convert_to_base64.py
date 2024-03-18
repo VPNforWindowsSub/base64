3568161fcd44659b9b9cf4ed2beffdd415a06d9c
VPNforWindowsSub <73077542+VPNforWindowsSub@users.noreply.github.com>
2024-03-18 21:02:50 +0330

    Update convert_to_base64.py

--- convert_to_base64.py
+++ convert_to_base64.py
@@ -1,9 +1,8 @@
 import base64
 import requests
 import os
-from github import Github
 
-def convert_to_base64(url, github_token):
+def convert_to_base64(url):
     # Download text file from the URL
     response = requests.get(url)
     if response.status_code == 200:
@@ -17,21 +16,9 @@ def convert_to_base64(url, github_token):
             f.write(encoded_text)
         print(f"Conversion complete. File saved to: {file_path}")
         print(f"Current working directory: {os.getcwd()}")
-
-        # Authenticate with GitHub using personal access token
-        g = Github(github_token)
-        repo = g.get_repo("VPNforWindowsSub/base64")
-
-        # Get main branch
-        branch = repo.get_branch("main")
-
-        # Create a new file in the repository
-        commit_message = "Add base64.txt"
-        repo.create_file("base64.txt", commit_message, encoded_text, branch="main")
     else:
         print("Failed to fetch data from URL.")
 
 if __name__ == "__main__":
     url = "https://raw.githubusercontent.com/dimzon/scaling-sniffle/main/all-sort.txt"
-    github_token = "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" 
-    convert_to_base64(url, github_token)
+    convert_to_base64(url)
