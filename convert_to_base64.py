87d0e2a1d3c43865801bca51d848692e94e00fb1
VPNforWindowsSub <khonamri.1383.mjj@gmail.com>
2024-03-18 20:28:00 +0330

    Add convert_to_base64.py script

---
+++ convert_to_base64.py
@@ -0,0 +1,21 @@
+import base64
+import requests
+
+def convert_to_base64(url):
+    # Download text file from the URL
+    response = requests.get(url)
+    if response.status_code == 200:
+        text = response.text
+        # Encode text to base64
+        encoded_bytes = base64.b64encode(text.encode('utf-8'))
+        encoded_text = encoded_bytes.decode('utf-8')
+        # Save base64-encoded text to base64.txt
+        with open('base64.txt', 'w') as f:
+            f.write(encoded_text)
+        print("Conversion complete.")
+    else:
+        print("Failed to fetch data from URL.")
+
+if __name__ == "__main__":
+    url = "https://raw.githubusercontent.com/dimzon/scaling-sniffle/main/all-sort.txt"
+    convert_to_base64(url)
