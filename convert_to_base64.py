4f3b69b9011362172c6f59dd5db0cb0a5e6672db
VPNforWindowsSub <73077542+VPNforWindowsSub@users.noreply.github.com>
2024-12-04 00:08:57 +0330

    Update convert_to_base64.py

--- convert_to_base64.py
+++ convert_to_base64.py
@@ -40,6 +40,7 @@ if __name__ == "__main__":
         "https://raw.githubusercontent.com/Space-00/V2ray-configs/refs/heads/main/config.txt",
         "https://raw.githubusercontent.com/MrMohebi/xray-proxy-grabber-telegram/master/collected-proxies/row-url/all.txt",
         "https://raw.githubusercontent.com/asakura42/vss/master/output.txt",
-        "https://raw.githubusercontent.com/qjlxg/ss/refs/heads/master/list_raw.txt"
+        "https://raw.githubusercontent.com/qjlxg/ss/refs/heads/master/list_raw.txt",
+        "https://raw.githubusercontent.com/snaCW/Config/refs/heads/main/config.txt"
     ]
     convert_multiple_to_base64(urls)
