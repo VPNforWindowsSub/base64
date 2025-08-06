d744033bb29dde73a42c923e0f7210932b071f5b
VPNforWindowsSub <73077542+VPNforWindowsSub@users.noreply.github.com>
2025-08-06 13:45:59 +0330

    Update convert_to_base64.py

--- convert_to_base64.py
+++ convert_to_base64.py
@@ -51,6 +51,7 @@ if __name__ == "__main__":
         "https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/ss.txt",
         "https://raw.githubusercontent.com/gfpcom/free-proxy-list/refs/heads/main/list/vmess.txt",
         "https://raw.githubusercontent.com/LalatinaHub/Mineral/refs/heads/master/result/nodes",
-        "https://raw.githubusercontent.com/snaCW/Config/refs/heads/main/config.txt"
+        "https://raw.githubusercontent.com/snaCW/Config/refs/heads/main/config.txt",
+        "https://raw.githubusercontent.com/Surfboardv2ray/TGParse/refs/heads/main/configtg.txt"
     ]
     convert_multiple_to_base64(urls)
